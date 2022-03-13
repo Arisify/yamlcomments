bde4e2b0749aa068bc9b583b5267e91bec76ac10
NTT1906 <nttispay@gmail.com>
2022-03-13 19:45:36 +0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # yamlcomments
-A PMMP virion pasting YAML comments
+A PMMP virion pasting YAML comments.
+
+## How does it work?
+It simply cuts through each line of Config and calculates things to find documents and comments. Then it saves those data back to Config if the old key is still there. You can use something like this.
+```php
+$config = new Config("path");
+$yaml_comments = new YamlComments($config);
+```
+
+## How do I save the comments/documents?
+```php
+$yaml_comments->save(); //If you want to save both config and documents
+$yaml_comments->saveDoc(); //If you want to save documents
+```
+Nothing happens when you don't save your config first, but I should tell you that if you save your config after save documents, the documents won't be saved.
+## How will this affect my server performance?
+Yesn't, this won't affect so much when the server is running stably, but it does require some cpu usage when you start or stop your server. 
+<br> But none or less, I don't recommend you using this for a producing server
+## What is doc and Inline doc?
+It doesn't make much sense, it's just that my understanding is so low that I don't know what to call it. It's like this:
+```yml
+---
+#This is a doc
+key: val #This is an Inline doc
+key1: val
+...
+```
+## Can I add documents for non-documented line?
+Yes, you can add it by this simply code:
+```php
+$key1 = "key1";
+$doc = "This is a doc for key1";
+$doc = "This is an inline doc for key1";
+$yaml_comments->setDoc($key1, $doc);
+$yaml_comments->setInlineDoc($key1, $inline_doc);
+```
+After savings the config and documents, the config file will appear to have something like this
+```yml
+---
+# This is a doc for key1
+key1: val #This is an inline doc for key1
+...
+```
+## How do I include this in other plugins
+If you use [Poggit](https://poggit.pmmp.io) to build your plugin, you can add it to your `.poggit.yml` like so:
+
+```yml
+projects:
+  YourPlugin:
+    libs:
+      - src: Arisify/yamlcomments/yamlcomments
+        version: ^1.0.0
+```
+
+## Are there any issues?
+Currently, I only know there was an issue where you can't add documents on the header and footer (this was intended because I thought this was not possible on YAML).
+<br> but if you found any issues, please report it [here](https://github.com/NTT1906/yamlcomments/issues)
---
+++ src/arie/yamlcomments/YamlComments.php
@@ -0,0 +1,216 @@
+<?php
+declare(strict_types=1);
+
+namespace arie\skywar\utils;
+
+use pocketmine\utils\Config;
+use Webmozart\PathUtil\Path;
+
+class YamlComments{
+	/** @var string */
+	private string $file;
+	/** @var Config */
+	private Config $config;
+	/** @var array */
+	private array $doc = [];
+	/** @var array */
+	private array $inline_doc = [];
+	/** @var bool */
+	private bool $supported;
+
+	public function __construct(Config $config){
+		$this->file = $config->getPath();
+		$this->supported = strtolower(Path::getExtension($this->file)) === "yml";
+		$this->config = $config;
+	}
+
+	public function isSupportedFile() : bool{
+		return $this->supported;
+	}
+
+	/**
+	 * This function will scan the YAML file for comments and stuff then save it in two different array, one for
+	 * documentations above it, one for documentations after it.
+	 *
+	 * Not recommended for production because lack of RAM eater (Note that this only takes a lot of CPU usage when the
+	 * server turn on and off)
+	 * @return void
+	 */
+	public function loadDoc() : void{
+		if (!$this->supported) {
+			return;
+		}
+		$lines = file($this->file, FILE_IGNORE_NEW_LINES);
+		$key = "";
+		$spaces = [];
+		$doc = [];
+		foreach ($lines as $line) {
+			if ($line === '...' || $line === '---') {
+				continue;
+			}
+			$l = ltrim($line);
+			$colon_pos = strpos($l, ':');
+			if (!isset($l[0])) { //Todo: $this->isBlank($line)?
+				$doc[] = "";
+				continue;
+			}
+			if ($l[0] === '#') {
+				$doc[] = $line;
+				continue;
+			}
+
+			if ($colon_pos === false) {
+				$val = str_replace([' ', '-'], '', $l);
+				$sharp_pos = strpos($val, '#');
+				if ($sharp_pos !== false) {
+					$val = mb_substr($val, 0, $sharp_pos);
+					$this->inline_doc[$key . "." . $val] = mb_substr($l, $sharp_pos);
+				}
+
+				if (!empty($doc)) {
+					$this->doc[$key . "." . $val] = $doc;
+					$doc = [];
+				}
+				continue;
+			}
+			$space = strlen($line) - strlen($l);
+
+			if ($space === 0) {
+				if ($line[0] !== '-') {
+					$key = mb_substr($l, 0, $colon_pos);
+				} else {
+					$key .= "." . str_replace([' ', '-'], '', mb_substr($l, 0, $colon_pos));
+				}
+			} else if ($spaces[$key] < $space) {
+				$key .= "." . str_replace([' ', '-'], '', mb_substr($l, 0, $colon_pos));
+			} else {
+				while($spaces[$key] >= $space) {
+					$last_dotpos = strrpos($key, '.');
+					if ($spaces[$key] === $space) {
+						$key = mb_substr($key, 0, $last_dotpos);
+						$key .= "." . str_replace([' ', '-'], '', mb_substr($l, 0, $colon_pos));
+						break; //This will stop the loop from checking for non-exist key...
+					}
+					$key = mb_substr($key, 0, $last_dotpos);
+				}
+			}
+
+			$spaces[$key] = $space;
+
+			if (!empty($doc)) {
+				$this->doc[$key] = $doc;
+				$doc = [];
+			}
+			$sharp_pos = strpos($l, '#');
+			if ($sharp_pos !== false) {
+				$this->inline_doc[$key] = mb_substr($l, $sharp_pos);
+			}
+		}
+	}
+
+	/**
+	 * @throws \JsonException
+	 */
+	public function saveConfig() : void{
+		$this->config->save();
+		$this->saveDoc();
+	}
+
+	/**
+	 * @param string $key
+	 * @return array|null
+	 */
+	public function getDoc(string $key) : ?array{
+		return $this->doc[$key] ?? null;
+	}
+
+	public function getDocParagraph(string $key) : ?string{
+		return isset($this->doc[$key]) ? implode(PHP_EOL, $this->doc[$key]) : null;
+	}
+
+	public function setDoc(string $key, array $doc = []) : void{
+		$this->doc[$key] = $doc;
+	}
+
+	public function addDoc(string $key, array $doc = []) : void{
+		$this->doc[$key] = array_merge($this->doc[$key] ?? [], $doc);
+	}
+
+	/**
+	 * @param string $key
+	 * @return string|null
+	 */
+	public function getInlineDoc(string $key) : ?string{
+		return $this->inline_doc[$key] ?? null;
+	}
+
+	public function setInlineDoc(string $key, string $doc) : void{
+		$this->inline_doc[$key] = $doc;
+	}
+
+	public function isBlank(string $line) : bool{
+		return preg_match('#^\s*$#', $line);
+	}
+
+	/**
+	 * This function will scan the YAML file again for keys and value, then check if there are comments of it in the data
+	 * If yes, the data will be parsed with the key and value
+	 *
+	 * Not recommended for production because lack of RAM eater (Note that this only takes a lot of CPU usage when the
+	 * server turn on and off)
+	 * @return void
+	 */
+	private function saveDoc() : void{
+		if (!$this->supported) {
+			return;
+		}
+		$lines = file($this->file, FILE_IGNORE_NEW_LINES);
+		$key = "";
+		$spaces = [];
+		$contents = "";
+		foreach ($lines as $line) {
+			$l = ltrim($line);
+			$colon_pos = strpos($l, ':');
+			if (!isset($l[0])) { //Todo: $this->isBlank($line)?
+				continue;
+			}
+
+			if ($colon_pos === false) {
+				$val = str_replace([' ', '-'], '', $l);
+				$sub_key = $key . "." . $val;
+				if (isset($this->doc[$sub_key])) {
+					$contents .= implode(PHP_EOL, $this->doc[$sub_key]) . PHP_EOL;
+				}
+				$contents .= $line . ($this->inline_doc[$sub_key] ?? "") . PHP_EOL;
+				continue;
+			}
+			$space = strlen($line) - strlen($l);
+			if ($space === 0) {
+				if ($line[0] !== '-') {
+					$key = mb_substr($l, 0, $colon_pos);
+				} else {
+					$key .= "." . str_replace([' ', '-'], '', mb_substr($l, 0, $colon_pos));
+				}
+			} else if ($spaces[$key] < $space) {
+				$key .= "." . str_replace([' ', '-'], '', mb_substr($l, 0, $colon_pos));
+			} else {
+				while ($spaces[$key] >= $space) {
+					$last_dotpos = strrpos($key, '.');
+					if ($spaces[$key] === $space) {
+						$key = mb_substr($key, 0, $last_dotpos);
+						$key .= "." . str_replace([' ', '-'], '', mb_substr($l, 0, $colon_pos));
+						break; //This will stop the loop from checking for non-exist key...
+					}
+					$key = mb_substr($key, 0, $last_dotpos);
+				}
+			}
+			$spaces[$key] = $space;
+
+			if (isset($this->doc[$key])) {
+				$contents .= $this->getDocParagraph($key) . PHP_EOL;
+			}
+			$contents .= $line . ($this->getInlineDoc($key) ?? "") . PHP_EOL;
+		}
+		file_put_contents($this->file, $contents);
+	}
+}
